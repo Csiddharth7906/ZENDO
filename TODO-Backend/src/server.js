0d2a932134a12303787e863c204cc7978ab04264
Siddharth Chauhan <siddharthchauhan7906@gmail.com>
2025-09-04 20:22:03 +0530

    Fix CORS headers for cookie authentication

--- TODO-Backend/src/server.js
+++ TODO-Backend/src/server.js
@@ -48,7 +48,7 @@ app.use(cors({
   origin: ['http://localhost:5173', 'https://zendo-two.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
   exposedHeaders: ['Set-Cookie']
 }));
 
